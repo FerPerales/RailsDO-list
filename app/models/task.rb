2853e352932305bbde16cf616c1b2bb331e6e26a
Fer Perales <fer.perales.montoya@gmail.com>
2012-12-26 20:15:21 -0600

    Change error message and rename date validation function

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,11 +1,11 @@
 class Task < ActiveRecord::Base
   attr_accessible :description, :dueDate
 
-  validates :description, :presence => {message: " A description of the task is requiered"}
+  validates :description, :presence => {message: " of the task is requiered"}
 
-  validate :expiration_date_cannot_be_in_the_past
+  validate :due_date_cannot_be_in_the_past
 
-  def expiration_date_cannot_be_in_the_past
+  def due_date_cannot_be_in_the_past
     if !dueDate.blank? and dueDate < Date.today
       errors.add(:dueDate, "can't be in the past")
     end
